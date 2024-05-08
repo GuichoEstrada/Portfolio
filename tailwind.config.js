cc87c47f2424dc4df900d8db62cb53576b079476
GuichoEstrada <lcrestrada.dev@gmail.com>
2024-05-07 20:10:23 -0700

    updated tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,12 @@ module.exports = {
         light: "#f5f5f5",
         primary: "#B63E96", 
         primaryDark: "#58E6D9", 
+      },
+      animation: {
+        'spin-slow': 'spin 20s linear infinite',
+      },
+      backgroundImage: {
+        circularLight: 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #f5f5f5 5px, #f5f5f5 100px);',
       }
     },
   },
