9fc07ebc19d2d400c011e1aa59703e86d49bdaac
GuichoEstrada <lcrestrada.dev@gmail.com>
2024-05-28 18:38:15 -0700

    added Logo color change effect on hover

---
+++ src/components/hooks/Logo.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+
+const MotionLink = motion(Link);
+
+const Logo = () => {
+  return (
+    <div className='flex items-center justify-center mt-2'>
+        <MotionLink 
+            href="/"
+            className='w-16 h-16 bg-dark text-light flex items-center justify-center dark:border-light border border-solid border-transparent
+            rounded-full text-2xl font-bold'
+            whileHover={{
+                backgroundColor:["#121212","#46B8AD","#204C4A","#46B8AD","#121212"],
+                transition:{duration:1, repeat: Infinity}
+            }}
+        >
+            LCE
+        </MotionLink>
+    </div>
+  )
+}
+
+export default Logo
